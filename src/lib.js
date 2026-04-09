17cad212a42b459c311d3f6edc788f16b2bcc2db
Gregor Kofler <info@gregorkofler.com>
2026-04-09 19:06:57 +0200

    Build: Remove composable from build export

--- src/lib.js
+++ src/lib.js
@@ -1,9 +1,7 @@
 import { stringToQrMatrix } from './stringToQrMatrix'
-import { useStringToQrMatrix  } from './useStringToQrMatrix'
 import QrCode from './QrCode.vue'
 
 export {
   stringToQrMatrix,
-  useStringToQrMatrix,
   QrCode
 }
